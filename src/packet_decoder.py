d7e8c9d409613e6c11e4a2fbb0361338deab242f
Alun Jones <alj118@aber.ac.uk>
2026-08-05 14:39:50 +0100

    Fix __getitem__ - super().__getitem__() doesn't exist, so don't call it!

--- src/packet_decoder.py
+++ src/packet_decoder.py
@@ -136,9 +136,7 @@ class PacketDecoder:
 
     def __getitem__(self, key: str) -> Any:
         """By-key retrieval."""
-        if key in self.fields:
-            return getattr(self, key)
-        return super().__getitem__(key)
+        return getattr(self, key)
 
     def keys(self) -> list[str]:
         """Dict-style "keys()" method."""
