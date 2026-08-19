655df9b75078d6591ac92179567d083c9b0e9543
Alun Jones <alj118@aber.ac.uk>
2026-08-19 15:02:20 +0100

    Update enfys_models with latest calibration data.

--- src/enfys_models.py
+++ src/enfys_models.py
@@ -9,14 +9,14 @@ class BB2ScienceDataSet(EnfysScienceDataSet):
 
     max_usable_adc_value = 60000
 
-    swir_wavelength_model = [ 0.1211, 614.2 ]
-    swir_low_to_medium_model = [ 29.4968, 0, 0 ]
-    swir_medium_to_high_model = [ 29.3227, 0, 0 ]
+    swir_wavelength_model = [ 0.1218, 607.8 ]
+    swir_low_to_medium_model = [ 29.5138, -610.6, 50 ]
+    swir_medium_to_high_model = [ 29.3979, -211.9, 50 ]
     swir_dac_offset_model = [ -48.771, 0 ]
 
-    mwir_wavelength_model = [ 0.2224, 1084.1 ]
-    mwir_low_to_medium_model = [ 29.4590, 0, 0 ]
-    mwir_medium_to_high_model = [ 29.5825, 0, 0 ]
+    mwir_wavelength_model = [ 0.2165, 1096.1 ]
+    mwir_low_to_medium_model = [ 29.5592, -1001.5, 61.4 ]
+    mwir_medium_to_high_model = [ 29.5533, -244.1, 109 ]
     mwir_dac_offset_model = [ -419.277, 0 ]
 
 class BB2EbScienceDataSet(BB2ScienceDataSet,EbEgseExtensions):
@@ -32,12 +32,12 @@ class EMScienceDataSet(EnfysScienceDataSet):
     name = "EM science data"
     max_usable_adc_value = 60000
 
-    swir_wavelength_model = [ 0.1225, 675.5 ]
+    swir_wavelength_model = [ 0.1218, 680.9 ]
     swir_low_to_medium_model = [ 29.5546, -792.3, 100 ]
     swir_medium_to_high_model = [ 29.3347, -272.1, 150 ]
     swir_dac_offset_model = [ -48.8356, 0 ]
 
-    mwir_wavelength_model = [ 0.2228, 1193.0 ]
+    mwir_wavelength_model = [ 0.2220, 1192.9 ]
     mwir_low_to_medium_model = [ 29.5842, -1265.2, 100 ]
     mwir_medium_to_high_model = [ 29.5825, -407.7, 100 ]
     mwir_dac_offset_model = [ -420.2187, 0 ]
